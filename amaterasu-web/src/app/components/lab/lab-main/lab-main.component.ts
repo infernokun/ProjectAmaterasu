740135e34f0aae4d2a3b82ef70a4bbbe7bb5c778
Javoris Brown <jbrown@-icr-team.com>
2025-04-07 16:00:06 -0600

    chaning up lab stuff web

--- amaterasu-web/src/app/components/lab/lab-main/lab-main.component.ts
+++ amaterasu-web/src/app/components/lab/lab-main/lab-main.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { Component, Input } from '@angular/core';
+import { Lab } from '../../../models/lab.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-lab-main',
   standalone: false,
   templateUrl: './lab-main.component.html',
-  styleUrl: './lab-main.component.scss'
+  styleUrl: './lab-main.component.scss',
+  animations: [
+    trigger('fadeIn', [
+        transition(':enter', [
+            style({ opacity: 0, transform: 'translateY(10px)' }),
+            animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+        ])
+    ])
+],
 })
 export class LabMainComponent {
+  labs: Lab[] = [];
 
+  @Input() labs$: Observable<Lab[] | undefined> | undefined;
+
+  isHovered: boolean = false;
+
+  isAlreadyDeployedByTeam: boolean = false;
+
+
+  onMouseEnter(): void {
+    this.isHovered = true;
+  }
+
+  onMouseLeave(): void {
+    this.isHovered = false;
+  }
+
+  deployLab(labId: string) {
+    
+  }
 }
