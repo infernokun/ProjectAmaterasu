dead04af94868c1944465e0503fabcef777d5e61
InfernoKun <infernokun@outlook.com>
2025-05-22 02:34:21 -0600

    small changes

---
+++ amaterasu-rest/src/main/java/com/infernokun/amaterasu/config/websocket/ClientConfig.java
@@ -0,0 +1,11 @@
+package com.infernokun.amaterasu.config.websocket;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+@Setter
+public class ClientConfig {
+}
